3adf25331fd60f57648a0ea723dc15283b21b470
Truongnd <nguyentruong281203@gmail.com>
2025-10-23 18:06:18 +0700

    Update Fuction Rewind

---
+++ Video Registers/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Video_Registers.Converters
+{
+    public class BooleanToVisibilityConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue && boolValue)
+            {
+                //true thì hiển thị ra 
+                return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+
+        }
+    }
+}
