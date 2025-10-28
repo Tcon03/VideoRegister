156da6cb670f3d667254f78b2d2bb6f66c27dc71
Truongnd <nguyentruong281203@gmail.com>
2025-10-28 20:06:27 +0700

    update ffmpeg

--- Register1/View/VideoRegister.xaml.cs
+++ Register1/View/VideoRegister.xaml.cs
@@ -187,7 +187,7 @@ namespace Register1.View
 
                 process.Start();
                 await process.WaitForExitAsync();
-
+               
 
 
                 if (saveImage?.imageList != null)
